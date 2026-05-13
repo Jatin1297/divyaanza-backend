86f8133209f7db2221da8793c5dcf6f78eb2a0c7
Jatin1297 <jatinsingh6547@gmail.com>
2026-05-14 00:28:38 +0530

    test deploy

--- controllers/adminAuthController.js
+++ controllers/adminAuthController.js
@@ -1,17 +1,13 @@
+import jwt from "jsonwebtoken";
+
 export const loginAdmin = async (req, res) => {
-
-  console.log("ENV EMAIL:", process.env.ADMIN_EMAIL);
-  console.log("ENV PASSWORD:", process.env.ADMIN_PASSWORD);
-
-  console.log("BODY:", req.body);
-
   const { email, password } = req.body;
 
   if (
     email !== process.env.ADMIN_EMAIL ||
     password !== process.env.ADMIN_PASSWORD
   ) {
-    return res.status(401).json({
+    return res.status(404).json({
       success: false,
       message: "Invalid email or password",
     });
